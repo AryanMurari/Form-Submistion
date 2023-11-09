cc18d00e78797257cb7eed47914e0a4580db3166
Aryan Murari <92181578+AryanMurari@users.noreply.github.com>
2023-11-09 18:24:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var firstName = document.getElementsByName('firstName')[0].value;
+    var lastName = document.getElementsByName('lastName')[0].value;
+
+    var data = {
+        firstName: firstName,
+        lastName: lastName
+    };
+
+    fetch('https://pkgedb89wj.execute-api.us-east-1.amazonaws.com/home', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+});
